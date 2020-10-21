573dd03af86bec1964d9aa63fa6f8a07f7a0609a
sidizein <sidi.wel.zein@gmail.com>
2020-10-21 20:04:48 +0200

    projet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { AuthGuardGuard } from './auth-guard.guard';
+import { AuthComponent } from './auth/auth.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'auth',component:AuthComponent},
+  {path:'admin',component:AdminComponent,
+  canActivate: [AuthGuardGuard],
+  children: [{
+    path:'',
+    loadChildren: () => import('./pages/page.module').then(m => m.PageModule )
+  }
+  ]
+},
+  {path:'',redirectTo:'/admin',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
